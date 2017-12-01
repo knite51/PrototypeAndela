24549d7dacd071afe15cf266663ff77e40e6956b
knite51 <knite51@gmail.com>
2017-12-01 15:17:44 +0100

    first oop function

---
+++ index.js
@@ -0,0 +1,43 @@
+class ShoppingCart {
+  constructor() {
+    this.total = 0;
+    this.items = {};
+  }
+  addItem(itemName, quantity, price) {
+    this.total += price * quantity;
+    if (this.items[itemName] !== undefined) {
+      this.items[itemName] += quantity;
+    } else {
+      this.items[itemName] = quantity;
+    }
+
+  }
+  removeItem(itemName, quantity, price) {
+    this.total -= price * quantity;
+    if (this.items.hasOwnProperty(itemName)) {
+      this.items[itemName] -= quantity;
+    } else {
+      delete this.items[itemName];
+    }
+  }
+  checkout(cashPaid) {
+    let balance = cashPaid - this.total;
+    if (balance >= 0) {
+      return balance;
+    } else {
+      balance = "Cash paid not enough";
+      return balance;
+    }
+  }
+}
+
+class Shop extends ShoppingCart {
+  constructor() {
+    super();
+    this.quantity = 100;
+  }
+  removeItem() {
+    this.quantity--;
+  }
+}
+
